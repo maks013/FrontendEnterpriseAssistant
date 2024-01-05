1f4dda39b901e94a8116a7eaad251d3782eef3cb
Maksymilian Ulanecki <maks.u@vp.pl>
2024-01-05 17:57:28 +0100

    added clients pages

---
+++ src/pages/clients/client-card/ClientCard.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './ClientCard.css';
+import ClientDetails from "../client-details/ClientDetails";
+import EditClient from "../edit-client/EditClient.js";
+
+const ClientCard = ({companyName, contactName, email}) => {
+    const [modalVisible, setModalVisible] = React.useState(false);
+    const [clientDetails, setClientDetails] = React.useState(null);
+    const [editModalVisible, setEditModalVisible] = React.useState(false);
+
+    const handleClientDetailsClick = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch(`http://localhost:8080/clients/byName/${companyName}`,{
+                method: 'GET',
+                    headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const details = await response.json();
+            setClientDetails(details);
+            setModalVisible(true);
+        } catch (error) {
+            console.error('Error fetching client details:', error);
+        }
+    };
+
+    const closeDetails = () => {
+        setModalVisible(false);
+    };
+
+    const handleEditClientDetails = () => {
+        setEditModalVisible(true);
+    };
+
+    const saveClientDetails = (updatedClient) => {
+        console.log(updatedClient);
+        setEditModalVisible(false);
+    };
+
+    return (
+        <div>
+            <div className="client-card">
+                <div className="company-name">{companyName}</div>
+                <div className="contact-name">{contactName}</div>
+                <div className="email">{email}</div>
+                <button className="details-button" onClick={() => handleClientDetailsClick()}>Szczegóły</button>
+            </div>
+            {modalVisible && (
+                <ClientDetails
+                    client={clientDetails}
+                    onClose={closeDetails}
+                    onEdit={handleEditClientDetails}
+                />
+            )}
+            {editModalVisible && (
+                <EditClient
+                    client={clientDetails}
+                    onSave={saveClientDetails}
+                    onClose={() => setEditModalVisible(false)}
+                />
+            )}
+        </div>
+    );
+};
+
+export default ClientCard;
---
+++ src/pages/clients/client-details/ClientDetails.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import './ClientDetails.css'
+import ConfirmationModal from '../../../components/confirmation/ConfirmationModal';
+
+const ClientDetails = ({client, onClose, onEdit}) => {
+    const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+
+    if (!client) return null;
+
+    const openConfirmModal = () => {
+        setIsConfirmModalOpen(true)
+    };
+    const closeConfirmModal = () => setIsConfirmModalOpen(false);
+
+    const handleDeleteClientClick = async () => {
+        openConfirmModal();
+    };
+
+    const confirmDeletion = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch(`http://localhost:8080/clients/${client.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+            if (response.status === 500) {
+                alert('Ten klient ma przypisane zamówienia i nie można go usunąć.')
+            } else {
+                throw new Error('Network response was not ok');
+            }
+            closeConfirmModal();
+        } catch (error) {
+            console.error('Error deleting client:', error);
+            closeConfirmModal();
+        }
+    };
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal">
+                <h2>{client.companyName}</h2>
+                <p>NIP: {client.taxIdNumber}</p>
+                <p>Adres: {client.address.street}</p>
+                <p>Miasto: {client.address.city}</p>
+                <p>Kod pocztowy: {client.address.postalCode}</p>
+                <p>Imię i nazwisko przedstawiciela: {client.representative.name} {client.representative.surname}</p>
+                <p>Nr telefonu: {client.representative.phoneNumber}</p>
+                <p>E-mail: {client.representative.email}</p>
+                <button className="edit-button" onClick={onEdit}>Edytuj dane</button>
+                <button className="delete-button" onClick={() => handleDeleteClientClick()}>Usuń klienta</button>
+                <button className="close-button" onClick={onClose}>Zamknij</button>
+            </div>
+            <ConfirmationModal
+                isOpen={isConfirmModalOpen}
+                message="Czy na pewno chcesz usunąć tego klienta?"
+                onConfirm={confirmDeletion}
+                onCancel={closeConfirmModal}
+            />
+        </div>
+    );
+};
+
+export default ClientDetails;
